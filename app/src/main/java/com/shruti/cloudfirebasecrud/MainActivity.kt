e3b1e4187d0f30c6aa269b33f1fb814e8c85dffc
Shruti sharma <105702669+48Shruti@users.noreply.github.com>
2024-04-09 16:17:25 +0530

    update and delete

--- app/src/main/java/com/shruti/cloudfirebasecrud/MainActivity.kt
+++ app/src/main/java/com/shruti/cloudfirebasecrud/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.shruti.cloudfirebasecrud
 
 import android.app.Dialog
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.Toast
@@ -19,7 +21,7 @@ class MainActivity : AppCompatActivity(),NotesInterface {
     var item = arrayListOf<NotesDataClass>()
     lateinit var adapter: RecyclerAdapter
     lateinit var linearLayout: LinearLayoutManager
-    val firebase = Firebase.firestore
+    val firestore = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -31,19 +33,18 @@ class MainActivity : AppCompatActivity(),NotesInterface {
     }
     fun fab(){
         val dialog = Dialog(this)
-        val dialogBinding = DialogCustomBinding.inflate(layoutInflater)
+        var dialogBinding = DialogCustomBinding.inflate(layoutInflater)
         dialog.setContentView(dialogBinding.root)
         dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT)
-        dialog.show()
         dialogBinding.btnadd.setOnClickListener {
             if(dialogBinding.ettitle.text.isNullOrEmpty()){
-                Toast.makeText(this,"Enter title",Toast.LENGTH_SHORT).show()
+                dialogBinding.ettitle.error = "Enter title"
             }
-            if(dialogBinding.etdescription.text.isNullOrEmpty()){
-                    Toast.makeText(this,"Enter description", Toast.LENGTH_SHORT).show()
+            else if(dialogBinding.etdescription.text.isNullOrEmpty()){
+                   dialogBinding.etdescription.error = "Enter description"
             }
             else {
-                firebase.collection("users")
+                firestore.collection("Users")
                     .add(
                         NotesDataClass(
                             title = dialogBinding.ettitle.text.toString(),
@@ -52,24 +53,28 @@ class MainActivity : AppCompatActivity(),NotesInterface {
                     )
                     .addOnSuccessListener {
                         Toast.makeText(this, "data add successfully", Toast.LENGTH_SHORT).show()
+                        System.out.println("its working ")
                         getCollection()
                     }
                     .addOnCanceledListener {
                         Toast.makeText(this, "data add cancel", Toast.LENGTH_SHORT).show()
+                        System.out.println("its canceled ")
                     }
-                    .addOnFailureListener {
+                    .addOnFailureListener { e->
                         Toast.makeText(this, "data add failure", Toast.LENGTH_SHORT).show()
+                        System.out.println("its failure ")
                     }
             }
                 adapter.notifyDataSetChanged()
                 dialog.dismiss()
 
         }
+        dialog.show()
 
     }
     private fun getCollection(){
         item.clear()
-        firebase.collection("user").get()
+        firestore.collection("Users").get()
             .addOnSuccessListener {
                 for(items in it.documents){
                     var firestore = items.toObject(NotesDataClass::class.java)?: NotesDataClass()
@@ -81,11 +86,58 @@ class MainActivity : AppCompatActivity(),NotesInterface {
     }
 
     override fun update(notesDataClass: NotesDataClass, position: Int) {
-        TODO("Not yet implemented")
+        val dialog = Dialog(this)
+        var dialogBinding = DialogCustomBinding.inflate(layoutInflater)
+        dialog.setContentView(dialogBinding.root)
+        dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialogBinding.btnadd.setOnClickListener {
+            if(dialogBinding.ettitle.text.isNullOrEmpty()){
+                dialogBinding.ettitle.error = "Enter title"
+            }
+            else if(dialogBinding.etdescription.text.isNullOrEmpty()){
+                dialogBinding.etdescription.error = "Enter description"
+            }
+            else {
+
+                    var updateNotes=
+                        NotesDataClass(
+                            title = dialogBinding.ettitle.text.toString(),
+                            description = dialogBinding.etdescription.text.toString()
+                        )
+                          firestore.collection("users").document(notesDataClass.id ?:"")
+                              .set(updateNotes)
+                    .addOnSuccessListener {
+                         Toast.makeText(this, "data add successfully", Toast.LENGTH_SHORT).show()
+                        getCollection()
+                    }
+                    .addOnCanceledListener {
+                        Toast.makeText(this, "data add cancel", Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnFailureListener { e->
+                        Toast.makeText(this, "data add failure", Toast.LENGTH_SHORT).show()
+                    }
+            }
+            adapter.notifyDataSetChanged()
+            dialog.dismiss()
+
+        }
+        dialog.show()
     }
 
     override fun delete(notesDataClass: NotesDataClass, position: Int) {
-        TODO("Not yet implemented")
+        firestore.collection("users").document(notesDataClass.id?:"")
+            .delete()
+            .addOnSuccessListener {
+                Toast.makeText(this,"Data delete",Toast.LENGTH_SHORT).show()
+                getCollection()
+            }
+            .addOnCanceledListener {
+                Toast.makeText(this,"Data Cancel",Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener{
+                Toast.makeText(this,"Data fail",Toast.LENGTH_SHORT).show()
+            }
+        adapter.notifyDataSetChanged()
     }
 
 }
